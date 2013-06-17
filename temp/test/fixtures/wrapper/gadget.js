77c0b637be6ff94c1c86ba16b22dcc9259d5b6ea
Andrey Mereskin <mereskin@gmail.com>
2013-06-17 16:55:20 +0400

    delete unused files

--- temp/test/fixtures/wrapper/gadget.js
+++
@@ -1,9 +0,0 @@
-define('gadget', ['cdn.backbone', 'cdn.jquery'], function(jquery, Backbone){
-  //jquery and backbone are used inside the gadget
-  return function(){}; // return constructor
-})
-
-define('gadget-dependency', ['cdn.backbone'], function(Backbone){
-  //dependency for gadget
-  return {};
-})
